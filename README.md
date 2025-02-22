7d9cf65b68ffb80376a9dec6a3ad89d4945a6894
RaphaelOdeareduo <odeareduor@mymacewan.ca>
2025-02-22 15:58:23 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# photographer-portfolio
+ A portfolio website made for a freelance photographer to showcase his skills. Built with React and TailwindCSS.
---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const rootEl = document.getElementById('root')
+createRoot(rootEl).render(<App />)
