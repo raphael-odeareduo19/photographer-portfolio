9d30923a1da6a0067bbc8e70d1fa157a95df077b
RaphaelOdeareduo <odeareduor@mymacewan.ca>
2025-02-28 13:30:07 -0700

    Update styling for project cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,8 +1,9 @@
 const ProjectCard = (props) => {
     return (
         <a href={props.projectLink} target="_blank">
-            <div className="shadow-[6px_6px_0_white] rounded overflow-hidden cursor-pointer">
-                <img className="w-full hover:scale-110 transition-all duration-500" src={props.projectImgSrc} alt="photgraphy-project" />
+            <div className="relative shadow-[6px_6px_0_white] rounded overflow-hidden cursor-pointer">
+                <div className="absolute w-full h-full text-white flex justify-center items-end text-2xl font-[Space_Grotesk] font-bold md:bg-[rgba(0,0,0,0.5)] hover:text-yellow-300 hover:bg-transparent transition-all duration-500">See more</div>
+                <img className="w-full" src={props.projectImgSrc} alt="photgraphy-project" />
             </div>
         </a>
     )
